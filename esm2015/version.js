e4e911897078eb878d27261815daf5b3c1074ad8
Edric Chan <edric.chan.1997@gmail.com>
2018-06-05 11:31:15 +0000

    master - 6f48452 fix(lib): update version

---
+++ esm2015/version.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { Version } from '@angular/core';
+export const /** @type {?} */ VERSION = new Version('1.0.0-alpha.1-6f48452');
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25neC15dGQtYXBpLyIsInNvdXJjZXMiOlsidmVyc2lvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUEsT0FBTyxFQUFFLE9BQU8sRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUV4QyxNQUFNLENBQUMsdUJBQU0sT0FBTyxHQUFHLElBQUksT0FBTyxDQUFDLGVBQWUsQ0FBQyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgVmVyc2lvbiB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5leHBvcnQgY29uc3QgVkVSU0lPTiA9IG5ldyBWZXJzaW9uKCcxLjAuMC1hbHBoYS4xJyk7XG4iXX0=
--- public_api.d.ts
+++ public_api.d.ts
@@ -1,3 +1,7 @@
 export * from './ytd-api.interfaces';
 export * from './ytd-api.service';
+/**
+ * @deprecated Use from the `search` submodule instead.
+ */
 export * from './search/public_api';
+export * from './version';
