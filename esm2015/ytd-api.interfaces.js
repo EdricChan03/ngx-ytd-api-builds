dff1049ee0c8547e84d6918a7e96cdbaa8d532f7
Edric Chan <edric.chan.1997@gmail.com>
2018-06-19 08:20:09 +0000

    master - 985595b fix(lib): change `apiKey` to `key`

--- esm2015/ytd-api.interfaces.js
+++ esm2015/ytd-api.interfaces.js
@@ -8,12 +8,8 @@
  */
 export function NgxYtdApiGenericOpts() { }
 function NgxYtdApiGenericOpts_tsickle_Closure_declarations() {
-    /**
-     * The API key.
-     * \@todo Add support for adding an API key via `forRoot` or `providers`
-     * @type {?}
-     */
-    NgxYtdApiGenericOpts.prototype.apiKey;
+    /** @type {?} */
+    NgxYtdApiGenericOpts.prototype.key;
 }
 /**
  * @record
@@ -32,4 +28,4 @@ function NgxYtdApiGenericResult_tsickle_Closure_declarations() {
     NgxYtdApiGenericResult.prototype.etag;
 }
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoieXRkLWFwaS5pbnRlcmZhY2VzLmpzIiwic291cmNlUm9vdCI6Im5nOi8vbmd4LXl0ZC1hcGkvIiwic291cmNlcyI6WyJ5dGQtYXBpLmludGVyZmFjZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJpYyBvcHRpb25zXG4gKi9cbmV4cG9ydCBpbnRlcmZhY2UgTmd4WXRkQXBpR2VuZXJpY09wdHMge1xuXHQvKipcblx0ICogVGhlIEFQSSBrZXkuXG5cdCAqIEB0b2RvIEFkZCBzdXBwb3J0IGZvciBhZGRpbmcgYW4gQVBJIGtleSB2aWEgYGZvclJvb3RgIG9yIGBwcm92aWRlcnNgXG5cdCAqL1xuXHRhcGlLZXk6IHN0cmluZztcbn1cblxuXG5leHBvcnQgaW50ZXJmYWNlIE5neFl0ZEFwaUdlbmVyaWNSZXN1bHQge1xuXHQvKipcblx0ICogVGhlIEFQSSdzIHJlc291cmNlIHR5cGVcblx0ICovXG5cdGtpbmQ/OiBzdHJpbmc7XG5cdC8qKlxuXHQgKiBUaGUgRVRhZyBvZiB0aGUgcmVxdWVzdFxuXHQgKi9cblx0ZXRhZz86IHN0cmluZztcbn1cbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoieXRkLWFwaS5pbnRlcmZhY2VzLmpzIiwic291cmNlUm9vdCI6Im5nOi8vbmd4LXl0ZC1hcGkvIiwic291cmNlcyI6WyJ5dGQtYXBpLmludGVyZmFjZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJpYyBvcHRpb25zXG4gKi9cbmV4cG9ydCBpbnRlcmZhY2UgTmd4WXRkQXBpR2VuZXJpY09wdHMge1xuXHRrZXk6IHN0cmluZztcbn1cblxuXG5leHBvcnQgaW50ZXJmYWNlIE5neFl0ZEFwaUdlbmVyaWNSZXN1bHQge1xuXHQvKipcblx0ICogVGhlIEFQSSdzIHJlc291cmNlIHR5cGVcblx0ICovXG5cdGtpbmQ/OiBzdHJpbmc7XG5cdC8qKlxuXHQgKiBUaGUgRVRhZyBvZiB0aGUgcmVxdWVzdFxuXHQgKi9cblx0ZXRhZz86IHN0cmluZztcbn1cbiJdfQ==
