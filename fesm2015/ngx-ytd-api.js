c8ca16541658b2dd4a4ffd5581d6aae225753c2a
Edric Chan <edric.chan.1997@gmail.com>
2019-05-01 08:02:41 +0000

    master - cef01af docs(lib/search): update code snippets to use `list` instead of the removed `search` method

--- fesm2015/ngx-ytd-api.js
+++ fesm2015/ngx-ytd-api.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('1.0.0-alpha.4-80647b8-80647b8');
+const VERSION = new Version('1.0.0-alpha.4-cef01af-cef01af');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/ngx-ytd-api.js
+++ fesm5/ngx-ytd-api.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('1.0.0-alpha.4-80647b8-80647b8');
+var VERSION = new Version('1.0.0-alpha.4-cef01af-cef01af');
 
 /**
  * @fileoverview added by tsickle
